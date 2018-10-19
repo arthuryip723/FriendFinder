a656eaf118eb3e485887bd139c8e90ab30df6819
Arthur Yip <ayip@spincar.com>
2018-10-19 12:53:44 -0400

    setup react

---
+++ frontend/index.jsx
@@ -0,0 +1,12 @@
+import { HashRouter } from 'react-router-dom';
+import React from 'react';
+import { Provider } from 'react-redux';
+import ReactDOM from 'react-dom';
+
+
+document.addEventListener('DOMContentLoaded', function () {
+    ReactDOM.render(
+        <h1>Hello Friends!</h1>,
+        document.getElementById('root')
+    );
+});
